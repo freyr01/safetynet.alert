cb240cec0aec7b2609d808be30ba34fa22c1eecd
Mathias Lauer <mathias.lauer.fr@gmail.com>
2021-01-22 17:03:06 +0100

    Add medical record test and improve age calculation logic

--- src/main/java/com/safetynet/alert/service/IMedicalRecordService.java
+++ src/main/java/com/safetynet/alert/service/IMedicalRecordService.java
@@ -2,6 +2,14 @@ package com.safetynet.alert.service;
 
 public interface IMedicalRecordService {
 	
+	/**
+	 * Calculate age based on birth date of a person
+	 * @param firstName
+	 * @param lastName
+	 * @return Age in years
+	 * @author Mathias Lauer
+	 * 22 janv. 2021
+	 */
 	public int getAgeOf(String firstName, String lastName);
 
 }
